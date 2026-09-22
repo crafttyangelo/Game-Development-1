3f118151a7afa5f858f08475b884846742f902ff
Angelo Apiado <131289007+crafttyangelo@users.noreply.github.com>
2026-09-22 12:21:52 -0400

    9/22

--- Game-Dev-1/Assets/Vectors/Turret.cs
+++ Game-Dev-1/Assets/Vectors/Turret.cs
@@ -10,6 +10,8 @@ public class Turret : MonoBehaviour
     private Vector3 bullet_dir = Vector3.zero;
     public float spread = 5f; // In degrees
 
+    public float view_angle = 25; // In degrees
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,9 +21,6 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Remove
-        bullet_dir = (target.transform.position - transform.position).normalized;
-        return;
 
         // If there's time left
         if (shoot_timer > 0)
@@ -39,10 +38,12 @@ public class Turret : MonoBehaviour
 
             // Get direction the bullet will travel in
             bullet_dir = (target.transform.position - transform.position).normalized;
+            Vector3 axis = Vector3.Cross(bullet_dir, Vector3.up);
+            Vector3 spread_dir = Quaternion.AngleAxis(Random.Range(0,spread), axis) * bullet_dir;
+            Vector3 final_dir = Quaternion.AngleAxis(Random.Range(0,360), bullet_dir) * spread_dir;
 
             // Pass the bullet direction to the new bullet we created
-            new_bullet.GetComponent<Bullet>().SetVelocity(bullet_dir);
-
+            new_bullet.GetComponent<Bullet>().SetVelocity(final_dir); 
         }
         
     }
@@ -53,7 +54,9 @@ public class Turret : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + bullet_dir * 2);
 
-        // Draw the up vector
+        #region Bullet Spread Cone
+
+        /*// Draw the up vector
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position, transform.position + Vector3.up * 2);
 
@@ -66,5 +69,33 @@ public class Turret : MonoBehaviour
         Gizmos.color = Color.yellow;
         Vector3 spread_dir = Quaternion.AngleAxis(spread, axis) * bullet_dir;
         Gizmos.DrawLine(transform.position, transform.position + spread_dir * 2);
+
+        // Show cone of possible directions for bullet to travel in
+        Gizmos.color = Color.magenta;
+        Vector3 final_dir;
+        for (int i = 1; i <= 15; i++)
+        {
+            final_dir = Quaternion.AngleAxis(22.5f * i, bullet_dir) * spread_dir;
+            Gizmos.DrawLine(transform.position, transform.position + final_dir * 2);
+        }*/
+
+        #endregion
+
+        #region Vision Cone
+
+        Gizmos.color = Color.beige;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 2);
+
+        float half_angle = view_angle / 2;
+
+        Vector3 right = Quaternion.AngleAxis(half_angle, Vector3.up) * transform.forward;
+        Vector3 left = Quaternion.AngleAxis(-half_angle, Vector3.up) * transform.forward;
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawLine(transform.position, transform.position + right * 10);
+        Gizmos.DrawLine(transform.position, transform.position + left * 10);
+
+
+        #endregion
     }
 }
